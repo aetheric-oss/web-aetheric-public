7d6704e79d5134a467c2d6e59077f9938075db64
Alex Smith <amsmith.pro@pm.me>
2025-07-14 19:46:08 +0200

    feat: add social media icons and adjust footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,3 +1,7 @@
+<script setup>
+import { Icon } from "@iconify/vue";
+</script>
+
 <template>
     <div class="container-fluid bg-gradient-img2">
         <div class="container-xl pt-5 pb-5 px-1 px-sm-3 px-xxl-5 text-light">
@@ -10,8 +14,8 @@
                     /><br />
                     <br />
                     <h5>
-                        An open source AAM ecosystem for flight planning and
-                        VTOL asset management, tailored for U-space.
+                        An open source AAM ecosystem for flight planning and VTOL asset management, tailored for
+                        U-space.
                     </h5>
                 </div>
             </div>
@@ -21,9 +25,11 @@
                     Lepelaar 6 A<br />
                     1628 CZ Hoorn<br />
                     Netherlands, Europe<br />
-
                     <br />
-                    <a href="mailto:info@aetheric.nl">info@aetheric.nl</a>
+
+                    <a id="footer-mailus" href="mailto:info@aetheric.nl"
+                        ><Icon style="font-size: 20px" icon="ph:envelope-simple-bold"></Icon> info@aetheric.nl</a
+                    >
                 </div>
                 <div class="col-12 col-sm-6 col-md-4">
                     <h4>About us</h4>
@@ -37,22 +43,49 @@
                 </div>
                 <div class="col-12 col-sm-6 col-md-4">
                     <h4>Find us</h4>
-                    <a href="https://github.com/aetheric-oss" target="_blank"
-                        >Visit our Github</a
-                    ><br />
+                    <br />
+                    <div id="footer-icon-bar">
+                        <a href="https://github.com/aetheric-oss" target="_blank">
+                            <Icon class="footer-icon" icon="hugeicons:github-01" />
+                        </a>
+                        <a href="https://www.linkedin.com/company/aetheric-bv" target="_blank">
+                            <Icon class="footer-icon" icon="hugeicons:linkedin-02" />
+                        </a>
+                        <a href="https://bsky.app/profile/aetheric-oss.bsky.social" target="_blank">
+                            <Icon class="footer-icon" icon="hugeicons:bluesky" />
+                        </a>
+                    </div>
+                    <br />
                 </div>
             </div>
         </div>
     </div>
-    <footer
-        class="container-fluid py-2 bg-dark text-light"
-    >
+    <footer class="container-fluid py-2 bg-dark text-light">
         <div class="container-xl">
             <div class="row">
-                <div class="col-12">
-                    © Copyright 2023 - 2024 • Aetheric • kvk 93258798
-                </div>
+                <div class="col-12">© Copyright 2023 - 2025 • Aetheric BV • KVK 93258798</div>
             </div>
         </div>
     </footer>
 </template>
+
+<style scoped>
+.row {
+    text-align: center;
+    justify-items: center;
+}
+
+#footer-mailus {
+    display: inline;
+    text-decoration: none;
+    text-transform: none;
+    justify-items: center;
+    align-items: center;
+}
+
+#footer-icon-bar .footer-icon {
+    font-size: 42px;
+    color: #fff;
+    margin-left: 10px;
+}
+</style>
